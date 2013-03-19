56717b7f0922459f6c5cb1a0ab8f3224bef14656
Lambi <whitedudeabove@gmail.com>
2013-03-19 10:54:48 +0100

    Guess the number!

---
+++ randomgenerator_guess.rb
@@ -0,0 +1,17 @@
+tal = rand(10)+1
+puts "Guess the number"
+guess = gets.to_i
+guesses = 1
+while guess != tal
+	if guess < tal
+		puts "Higher" 
+		puts "Guess again"
+	else
+		puts "Lower" 
+		puts "Guess again"
+	end
+	guess = gets.to_i	
+	guesses = guesses + 1
+end
+puts "Correct!"
+puts "Number of guesses:#{guesses}"
